5127881f7ba62dbd752e5796d27ab8611996007a
Jabar <95591582+jabarRr@users.noreply.github.com>
2026-04-15 02:25:46 +0100

    intial commit + webpack structure done

---
+++ webpack.common.js
@@ -0,0 +1,31 @@
+ import path from 'node:path';
+ import { fileURLToPath } from 'node:url';
+ import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+ const __filename = fileURLToPath(import.meta.url);
+ const __dirname = path.dirname(__filename);
+
+ export default {
+   entry: {
+     app: './src/index.js',
+   },
+   plugins: [
+     new HtmlWebpackPlugin({
+       title: 'Production',
+       template: "./src/template.html",
+     }),
+   ],
+   output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+   },
+   module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+ };
